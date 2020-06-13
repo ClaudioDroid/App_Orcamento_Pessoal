358fed0696a64ee190c65d43f0afef295db6beb9
ClaudioDroid <claudio.v.lima2@gmail.com>
2020-06-13 01:08:33 -0300

    verificando se os campos foram preenchidos

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ class Despesa{
     this.descricao = descricao;
     this.valor = valor;
   }
+
+  validaCampos(){
+    for(let i in this){ // Possível bug na checagem dos itens preenchidos 
+      if(this[i] == undefined || this[i] == null || this[i] == ''){
+        return false;
+      }else{
+        return true;
+      }
+    }
+  }
 }
 
 class Bd{
@@ -47,9 +57,36 @@ function cadastrarDespesa(){
     descricao.value,
     valor.value
   )
-  bd.gravar(despesa);
+  
+  if (despesa.validaCampos()){
+    bd.gravar(despesa);
 
+    document.getElementById('exampleModalLabel').innerHTML = 'Cadastrado com sucesso'
+    document.getElementById('modal-bodyId').innerHTML = 'Os dados foram cadastrado com sucesso!'
+
+    // document.getElementById('modal-headerId').classList.remove('text-danger')
+    // document.getElementById('btnVoltar').classList.remove('btn-danger');
+    document.getElementById('modal-headerId').className = 'modal-header text-success';
+    document.getElementById('btnVoltar').className = 'btn-success';
+
+    $('#registraDespesa').modal('show');
+    console.log('Dados válidos!');
+  }else{
+    document.getElementById('exampleModalLabel').innerHTML = 'Erro no cadastro'
+    document.getElementById('modal-bodyId').innerHTML = 'Existem campos que não foram preenchidos!'
+
+    document.getElementById('modal-headerId').className = 'modal-header text-danger';
+    document.getElementById('btnVoltar').className = 'btn-danger';
+
+    // document.getElementById('modal-headerId').classList.remove('text-success')
+    // document.getElementById('btnVoltar').classList.remove('btn-success');
+    // document.getElementById('modal-headerId').classList.add('text-danger')
+    // document.getElementById('btnVoltar').classList.add('btn-danger');
+    $('#registraDespesa').modal('show');
+  }
 }
+
+
 
 
 
